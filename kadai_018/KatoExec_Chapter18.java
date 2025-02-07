83fd6222c7f257b178e35aaea0897baebfa948ed
石田丈 <jo1485190828@icloud.com>
2025-02-07 13:03:40 +0900

    Add files via upload

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,19 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+
+	public static void main(String[] args) {
+		KatoTaro_Chapter18 KatoTaro = new KatoTaro_Chapter18();
+		KatoTaro.setGivenName();
+		KatoTaro.execIntroduce();
+		
+		KatoIchiro_Chapter18 KatoIchiro = new KatoIchiro_Chapter18();
+		KatoIchiro.setGivenName();
+		KatoIchiro.execIntroduce();
+		
+		KatoHanako_Chapter18 KatoHanako = new KatoHanako_Chapter18();
+		KatoHanako.setGivenName();
+		KatoHanako.execIntroduce();
+				
+	}
+}
